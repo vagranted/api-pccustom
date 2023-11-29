0ecccb85171d0e05acde7c4a4d6b70dd682bc7bf
Человек <nik21.dance@gmail.com>
2023-11-29 16:13:28 +0700

    рефакторинг миграции users

--- database/migrations/2023_11_29_065332_create_users_table.php
+++ database/migrations/2023_11_29_065332_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedTinyInteger('role_id');
+            $table->unsignedTinyInteger('role_id')->default(1);
             $table->string('login')->unique();
             $table->string('password');
 
